ca6fe9246663dd3ca7f0a28fab4ab962718b6826
dev-rome <haynesjerome@gmail.com>
2023-04-20 22:42:40 -0400

    Integrate ThemeProvider into custom _app for global theme support

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,13 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import { ThemeProvider } from "@/context/ThemeContext";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider>
+      <main className="bg-[#f8f9fa] dark:bg-[#212529] min-h-screen">
+        <Component {...pageProps} />
+      </main>
+    </ThemeProvider>
+  );
 }
