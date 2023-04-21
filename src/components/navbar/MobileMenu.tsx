6321f7514f3416a624836d0910c8091c12aefdd3
dev-rome <haynesjerome@gmail.com>
2023-04-21 13:46:49 -0400

    added mobile and desktop style nav

---
+++ src/components/navbar/MobileMenu.tsx
@@ -0,0 +1,103 @@
+import React, { useState } from "react";
+import { motion, Variants } from "framer-motion";
+import { RiMenu3Line, RiCloseLine } from "react-icons/ri";
+import { FaHome, FaUser, FaBriefcase, FaEnvelope } from "react-icons/fa";
+
+type NavItem = {
+  target: string;
+  icon: React.ReactNode;
+};
+
+const navItems: NavItem[] = [
+  {
+    target: "#home",
+    icon: <FaHome size={24} />,
+  },
+  {
+    target: "#about",
+    icon: <FaUser size={24} />,
+  },
+  {
+    target: "#portfolio",
+    icon: <FaBriefcase size={24} />,
+  },
+  {
+    target: "#contact",
+    icon: <FaEnvelope size={24} />,
+  },
+];
+
+const MobileMenu = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
+  const handleMenuItemClick = (target: string) => {
+    toggleMenu();
+    const element = document.querySelector(target);
+    element?.scrollIntoView({ behavior: "smooth" });
+  };
+
+  const circleVariants: Variants = {
+    open: {
+      opacity: 1,
+      transition: {
+        staggerChildren: 0.1,
+        delayChildren: 0.1,
+      },
+    },
+    closed: {
+      opacity: 0,
+      transition: {
+        staggerChildren: 0.1,
+        staggerDirection: -1,
+      },
+    },
+  };
+
+  const itemVariants: Variants = {
+    open: {
+      opacity: 1,
+      y: 0,
+    },
+    closed: {
+      opacity: 0,
+      y: 50,
+    },
+  };
+
+  return (
+    <div className="sm:hidden fixed bottom-4 right-4">
+      <motion.div
+        initial={{ opacity: 0, scale: 0 }}
+        animate={{ opacity: 1, scale: 1 }}
+        exit={{ opacity: 0, scale: 0 }}
+        onClick={toggleMenu}
+      >
+        <button className="p-3 rounded-full dark:bg-[#7AB9F0] bg-[#0D4373] text-white">
+          {isOpen ? <RiCloseLine size={24} /> : <RiMenu3Line size={24} />}
+        </button>
+        <motion.div
+          className="absolute bottom-12 right-4 flex items-center gap-4"
+          variants={circleVariants}
+          animate={isOpen ? "open" : "closed"}
+        >
+          {navItems.map((item, index) => (
+            <motion.button
+              key={index}
+              className="p-2 rounded-full dark:bg-[#7AB9F0] bg-[#0D4373] text-white"
+              onClick={() => handleMenuItemClick(item.target)}
+              variants={itemVariants}
+            >
+              {item.icon}
+            </motion.button>
+          ))}
+        </motion.div>
+      </motion.div>
+    </div>
+  );
+};
+
+export default MobileMenu;
---
+++ src/components/navbar/NavMenu.tsx
@@ -0,0 +1,84 @@
+import {
+  FaUser,
+  FaBriefcase,
+  FaHome,
+  FaEnvelope,
+  FaGithub,
+  FaLinkedin,
+} from "react-icons/fa";
+import Link from "next/link";
+
+type NavigationItem = {
+  title: string;
+  icon: React.ReactNode;
+  href: string;
+};
+
+const navigationItems: NavigationItem[] = [
+  { title: "Home", icon: <FaHome />, href: "#home" },
+  { title: "About", icon: <FaUser />, href: "#about" },
+  { title: "Portfolio", icon: <FaBriefcase />, href: "#portfolio" },
+  { title: "Contact", icon: <FaEnvelope />, href: "#contact" },
+];
+
+const handleClick = (e: React.MouseEvent<HTMLAnchorElement>) => {
+  e.preventDefault();
+  const sectionId = e.currentTarget.getAttribute("href");
+  const sectionElement = document.querySelector<HTMLElement>(sectionId!);
+  if (sectionElement) {
+    const sectionPosition = sectionElement.offsetTop;
+    window.scrollTo({ top: sectionPosition, behavior: "smooth" });
+  }
+};
+
+const Navmenu = () => {
+  return (
+    <nav className="flex h-screen">
+      <div className="w-44 dark:bg-[#7AB9F0] bg-[#0D4373] flex flex-col justify-between">
+        <div>
+          <div className="flex items-center justify-center mt-8">
+            <p className="text-white">JH</p>
+          </div>
+
+          <ul className="mt-8 flex flex-col justify-center flex-grow">
+            {navigationItems.map((item, index) => (
+              <li key={index} className="relative my-2">
+                <a
+                  href={item.href}
+                  className="block py-2 px-4 text-white text-lg rounded-lg hover:bg-blue-500 hover:bg-opacity-50 transition-colors duration-300"
+                  onClick={handleClick}
+                >
+                  <div className="flex items-center">
+                    {item.icon}
+                    <span className="ml-4">{item.title}</span>
+                  </div>
+                </a>
+              </li>
+            ))}
+          </ul>
+        </div>
+
+        <div className="flex justify-center mb-8">
+          <Link
+            href="https://github.com/"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-white mr-4"
+          >
+            <FaGithub size={20} />
+          </Link>
+          <Link
+            href="https://linkedin.com/"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-white"
+          >
+            <FaLinkedin size={20} />
+          </Link>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navmenu;
