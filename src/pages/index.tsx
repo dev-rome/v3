396d146180e59e8a64bb99a3ed1bfbf6575621b4
dev-rome <haynesjerome@gmail.com>
2023-04-22 14:48:38 -0400

    imported next link for page navigation

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,5 @@
-import Link from "next/link";
 import { motion } from "framer-motion";
+import Link from "next/link";
 import AnimatedImageSVG from "@/components/AnimatedImageSVG";
 import ThemeToogle from "@/components/ThemeToggle";
 
