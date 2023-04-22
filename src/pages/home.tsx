befde629b551072ebc24ec6e4c8308d4424e2039
dev-rome <haynesjerome@gmail.com>
2023-04-22 14:51:08 -0400

    added motion div for page transition effect, and thmetoggle to change from dark to light theme, adn imports to show home page content

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,15 +1,27 @@
+import { motion } from "framer-motion";
 import Layout from "@/components/Layout";
-import ThemeToogle from "@/components/ThemeToggle";
+import Hero from "@/components/hero/Hero";
+import About from "@/components/about/About";
 import MobileMenu from "@/components/navbar/MobileMenu";
+import ThemeToogle from "@/components/ThemeToggle";
 
 function Home() {
   return (
-    <>
+    <motion.div
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      transition={{
+        duration: 1,
+        ease: "easeInOut",
+      }}
+    >
       <Layout>
+      <ThemeToogle />
         <MobileMenu />
-        <div>home</div>
+        <Hero />
+        <About />
       </Layout>
-    </>
+    </motion.div>
   );
 }
 
