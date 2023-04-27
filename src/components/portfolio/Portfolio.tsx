8dfc429995c29f451eab150d3299c4a79c5598b4
dev-rome <haynesjerome@gmail.com>
2023-04-27 10:57:39 -0400

    added state to manage read more button on images and to help with mobile

--- src/components/portfolio/Portfolio.tsx
+++ src/components/portfolio/Portfolio.tsx
@@ -6,6 +6,7 @@ import Modal from "../Modal";
 
 function Portfolio() {
   const [selectedProject, setSelectedProject] = useState<number | null>(null);
+  const [showReadMoreIndex, setShowReadMoreIndex] = useState<number | null>(null);
 
   const handleModalOpen = (index: number) => {
     setSelectedProject(index - 1);
@@ -15,18 +16,28 @@ function Portfolio() {
     setSelectedProject(null);
   };
 
+  const handleImageClick = (index: number) => {
+    if (showReadMoreIndex === index) {
+      setShowReadMoreIndex(null);
+    } else {
+      setShowReadMoreIndex(index);
+    }
+  };
+
   return (
     <section id="portfolio" className="pb-32 lg:pb-64">
       <h2 className="text-[#0D4373] dark:text-[#7AB9F0] text-md mb-2">
         Portfolio
       </h2>
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3">
-        {PortfolioData.map((project) => {
+        {PortfolioData.map((project, index) => {
           return (
             <PortfolioItem
               key={project.id}
               project={project}
               onClick={() => handleModalOpen(project.id)}
+              onImageClick={() => handleImageClick(index)}
+              showReadMore={showReadMoreIndex === index}
             />
           );
         })}
--- src/components/portfolio/PortfolioItem.tsx
+++ src/components/portfolio/PortfolioItem.tsx
@@ -7,32 +7,42 @@ interface PortfolioItemProps {
     alt: string;
   };
   onClick: () => void;
+  onImageClick: () => void;
+  showReadMore: boolean;
 }
 
-const PortfolioItem = ({ project, onClick }: PortfolioItemProps) => {
+const PortfolioItem = ({
+  project,
+  onClick,
+  onImageClick,
+  showReadMore,
+}: PortfolioItemProps) => {
   return (
-    <div className="relative overflow-hidden">
+    <div className="relative overflow-hidden cursor-pointer">
       <Image
         className="mx-auto"
         width={500}
         height={500}
         src={project.src}
         alt={project.alt}
+        onClick={onImageClick}
       />
-      <motion.div
-        className="absolute inset-0 bg-black bg-opacity-30 items-center justify-center flex"
-        initial={{ opacity: 0 }}
-        whileHover={{ opacity: 1 }}
-        transition={{ duration: 0.3 }}
-      >
-        <motion.button
-          className="dark:bg-[#7AB9F0] bg-[#0D4373] text-[#f8f9fa] p-3 w-24 h-24 rounded-full text-xs font-bold"
-          whileHover={{ scale: 1.1 }}
-          onClick={onClick}
+      {showReadMore && (
+        <motion.div
+          className="absolute inset-0 bg-black bg-opacity-30 items-center justify-center flex"
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ duration: 0.3 }}
         >
-          Read More
-        </motion.button>
-      </motion.div>
+          <motion.button
+            className="dark:bg-[#7AB9F0] bg-[#0D4373] text-[#f8f9fa] p-3 w-24 h-24 rounded-full text-xs font-bold"
+            whileHover={{ scale: 1.1 }}
+            onClick={onClick}
+          >
+            Read More
+          </motion.button>
+        </motion.div>
+      )}
     </div>
   );
 };
