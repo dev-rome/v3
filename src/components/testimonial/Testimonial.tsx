421ca7d1d9f82eeb1a5fc5b3b122272d2219ed79
dev-rome <haynesjerome@gmail.com>
2023-06-08 10:53:00 -0400

    added testimonial component with testimonial data

---
+++ src/components/testimonial/Testimonial.tsx
@@ -0,0 +1,25 @@
+import { ImQuotesLeft } from "react-icons/im";
+import { TestimonialData } from "./TestimonialData";
+
+export default function Testimonial() {
+  return (
+    <section id="testimonial" className="pb-32 lg:pb-64">
+      <h2 className="text-[#0D4373] dark:text-[#7AB9F0] text-md mb-2">
+        Testimonial
+      </h2>
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-10">
+        {TestimonialData.map(({ quote, author, id }) => (
+          <div className="relative bg-[#0D4373] dark:bg-[#7AB9F0] p-8" key={id}>
+            <div className="absolute top-5 left-8">
+              <ImQuotesLeft className="text-[#7AB9F0] dark:text-[#0D4373] opacity-20 dark:opacity-10 text-7xl" />
+            </div>
+            <p className="text-[#f8f9fa]">
+              {quote}
+            </p>
+            <p className="text-[#f8f9fa]">{author}</p>
+          </div>
+        ))}
+      </div>
+    </section>
+  );
+}
---
+++ src/components/testimonial/TestimonialData.tsx
@@ -0,0 +1,20 @@
+export const TestimonialData = [
+  {
+    id: 1,
+    quote:
+      "I am very satisfied with the website that Jerome created for my business. It looks professional and has helped me attract more customers online. I appreciate Jerome's attention to detail and prompt response to my requests. I would definitely recommend his services.",
+    author: "Charnelle D.",
+  },
+  {
+    id: 2,
+    quote:
+      "I'm really happy with the website Jerome designed for me. It's simple, user-friendly, and has all the information my clients need. Jerome was great to work with, he understood my vision and delivered a website that exceeded my expectations. I would definitely work with him again in the future.",
+    author: "Luis S.",
+  },
+  {
+    id: 3,
+    quote:
+      "Jerome did an excellent job building my website. It's clean, organized, and easy to navigate. I've received positive feedback from my customers about the design. Jerome was professional, patient, and accommodating throughout the process. I highly recommend him for website design services.",
+    author: "Michael R.",
+  },
+];
