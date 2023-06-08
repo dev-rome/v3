48774ab3d55eb7da2d697768754ff6faea69c2ed
dev-rome <haynesjerome@gmail.com>
2023-06-08 13:30:03 -0400

    edit email form

--- src/components/contact/Contact.tsx
+++ src/components/contact/Contact.tsx
@@ -36,40 +36,44 @@ export default function Contact() {
         >
           <div className="max-w-xl mx-auto flex flex-col">
             <div className="flex gap-5">
-              <input
-                type="text"
-                placeholder="Name"
-                {...register("name", {
-                  required: true,
-                  maxLength: 100,
-                })}
-                className="w-full bg-transparent border-b border-[#0D4373] dark:border-[#7AB9F0] placeholder:text-[#212529] placeholder:dark:text-[#f8f9fa] text-[#212529] dark:text-[#f8f9fa] p-3 outline-none"
-              />
-              {errors.name && (
-                <span className="text-[#e03131] mt-1">
-                  {errors.name.type === "required" && "Name is required"}
-                  {errors.name.type === "maxLength" &&
-                    "Max length is 100 characters"}
-                </span>
-              )}
-              <input
-                type="email"
-                placeholder="Email"
-                {...register("email", {
-                  required: true,
-                  pattern: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
-                })}
-                className="w-full bg-transparent border-b border-[#0D4373] dark:border-[#7AB9F0] placeholder:text-[#212529] placeholder:dark:text-[#f8f9fa] text-[#212529] dark:text-[#f8f9fa] p-3 focus:outline-none"
-              />
-              {errors.email && (
-                <span className="text-[#e03131] mt-1">
-                  {errors.email.type === "required" && "Email is required"}
-                  {errors.email.type === "pattern" &&
-                    "Please enter a valid email address"}
-                </span>
-              )}
+              <div className="flex flex-col w-full">
+                <input
+                  type="text"
+                  placeholder="Name"
+                  {...register("name", {
+                    required: true,
+                    maxLength: 100,
+                  })}
+                  className="w-full bg-transparent border-b border-[#0D4373] dark:border-[#7AB9F0] placeholder:text-[#212529] placeholder:dark:text-[#f8f9fa] text-[#212529] dark:text-[#f8f9fa] p-3 outline-none"
+                />
+                {errors.name && (
+                  <span className="flex text-[#e03131] text-xs mt-1">
+                    {errors.name.type === "required" && "Name is required"}
+                    {errors.name.type === "maxLength" &&
+                      "Max length is 100 characters"}
+                  </span>
+                )}
+              </div>
+              <div className="flex flex-col w-full">
+                <input
+                  type="email"
+                  placeholder="Email"
+                  {...register("email", {
+                    required: true,
+                    pattern: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+                  })}
+                  className="w-full bg-transparent border-b border-[#0D4373] dark:border-[#7AB9F0] placeholder:text-[#212529] placeholder:dark:text-[#f8f9fa] text-[#212529] dark:text-[#f8f9fa] p-3 focus:outline-none"
+                />
+                {errors.email && (
+                  <span className="flex text-[#e03131] text-xs mt-1">
+                    {errors.email.type === "required" && "Email is required"}
+                    {errors.email.type === "pattern" &&
+                      "Please enter a valid email address"}
+                  </span>
+                )}
+              </div>
             </div>
-            <div>
+            <div className="flex flex-col">
               <textarea
                 typeof="text"
                 placeholder="Message"
@@ -82,20 +86,19 @@ export default function Contact() {
                 className="w-full bg-transparent border-b border-[#0D4373] dark:border-[#7AB9F0] placeholder:text-[#212529] placeholder:dark:text-[#f8f9fa] text-[#212529] dark:text-[#f8f9fa] focus:outline-none resize-none p-3 mt-8"
               />
               {errors.message && (
-                <span className="text-[#e03131] mt-1">
+                <span className="flex text-[#e03131] text-xs mt-1">
                   {errors.message.type === "required" && "Email is required"}
                   {errors.message.type === "maxLength" &&
                     "Max length is 1000 characters"}
                 </span>
               )}
             </div>
-            <div className="flex justify-end">
+            <div className="flex justify-end mt-3">
               <motion.button
                 type="submit"
                 className="text-white bg-[#0D4373] dark:bg-[#7AB9F0] py-1 px-3"
                 whileHover={{ scale: 1.1 }}
                 transition={{ type: "spring", stiffness: 400, damping: 10 }}
-                onClick={() => reset()}
               >
                 Sumbit
               </motion.button>
