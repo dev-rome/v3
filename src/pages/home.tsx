09a51c3763703cc5a18138c2994b0bdd947f1d1b
dev-rome <haynesjerome@gmail.com>
2023-06-13 14:04:53 -0400

    added meta data

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -5,21 +5,25 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>Jerome Haynes</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+        <meta charSet="UTF-8" />
+        <meta name="language" content="english" />
+        <meta name="robots" content="index, follow" /> 
         <meta
           name="description"
-          content="Passionate frontend/software engineer leveraging the web to connect people, create amazing experiences, and turn ideas into reality. Explore my portfolio and let's achieve your goals together."
+          content="Passionate Frontend/Software Engineer based in New York, leveraging the web to connect people, create amazing experiences, and turn ideas into reality. Explore my portfolio and let's achieve your goals together."
         />
         <meta name="author" content="Jerome Haynes" />
 
         <meta property="og:title" content="Jerome Haynes" />
         <meta
           property="og:description"
-          content="Passionate frontend/software engineer leveraging the web to connect people, create amazing experiences, and turn ideas into reality. Explore my portfolio and let's achieve your goals together."
+          content="Passionate Frontend/Software Engineer based in New York, leveraging the web to connect people, create amazing experiences, and turn ideas into reality. Explore my portfolio and let's achieve your goals together."
         />
-        <meta property="og:url" content="https://jeromehaynes.com/" />
+        <meta property="og:url" content="https://jeromehaynes.com/home" />
         <meta property="og:type" content="website" />
-        <title>Jerome Haynes</title>
+        <title>Jerome Haynes - Frontend/Software Engineer</title>
       </Head>
       <HomePage />
     </>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,7 +5,28 @@ export default function Welcome() {
   return (
     <>
       <Head>
-        <title>Jerome Haynes</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+        <meta charSet="UTF-8" />
+        <meta name="language" content="english" />
+        <meta name="robots" content="index" /> 
+        <meta
+          name="description"
+          content="Welcome to Jerome Haynes' portfolio website. Jerome is a Frontend/Software Engineer with expertise in web development."
+        />
+        <meta name="author" content="Jerome Haynes" />
+
+        <meta
+          property="og:title"
+          content="Jerome Haynes - Frontend/Software Engineer"
+        />
+        <meta
+          property="og:description"
+          content="Welcome to Jerome Haynes' portfolio website. Jerome is a Frontend/Software Engineer with expertise in web development."
+        />
+        <meta property="og:url" content="https://jeromehaynes.com/" />
+        <meta property="og:type" content="website" />
+        <title>Jerome Haynes - Frontend/Software Engineer</title>
       </Head>
 
       <WelcomePage />
