afcc7b55cb9f2962d5e672d48ff93b2a812d6920
dev-rome <haynesjerome@gmail.com>
2023-08-13 18:58:34 -0400

    removed projects

--- src/components/portfolio/PortfolioData.tsx
+++ src/components/portfolio/PortfolioData.tsx
@@ -71,15 +71,6 @@ export const PortfolioData = [
   },
   {
     id: 8,
-    src: "/images/todo-app.png",
-    alt: "Todo App",
-    title: "Todo App",
-    description:
-      "This project is a Todo application built with Next.js, TypeScript, and Tailwind CSS. It is a personal project aimed at mastering Next.js and TypeScript through practical implementation.",
-    github: "https://github.com/dev-rome/NextJS-Todo-App",
-  },
-  {
-    id: 9,
     src: "/images/video-game-trivia.png",
     alt: "Video Game Trivia",
     title: "Video Game Trivia",
@@ -87,35 +78,5 @@ export const PortfolioData = [
       "A basic trivia game built with HTML, CSS, and JavaScript, using the open trivia API to fetch trivia game questions and answers.",
     github: "https://github.com/dev-rome/dev-rome.github.io",
     live: "https://dev-rome.github.io/",
-  },
-  {
-    id: 10,
-    src: "/images/tenant-estate.png",
-    alt: "Tenant Estate",
-    title: "Tenant Estate",
-    description:
-      "A home/apartment listing app that allows users to view, add, update, and delete using full CRUD functionality built with Express, Mongoose, MongoDB, EJS, JavaScript, CSS, and BootStrap.",
-    github: "https://github.com/dev-rome/Tenant-Estate",
-    live: "https://enigmatic-anchorage-66839.herokuapp.com/buy",
-  },
-  {
-    id: 11,
-    src: "/images/gameoverflow.png",
-    alt: "GameOverflow",
-    title: "GameOverflow",
-    description:
-      "Q&A gaming app that allows users to post their questions on games that the users play, also users can also answer questions to help the user fix their problems built with Python, Django.",
-    github: "https://github.com/dev-rome/Gameoverflow",
-    live: "https://guarded-plateau-67104.herokuapp.com/",
-  },
-  {
-    id: 12,
-    src: "/images/profolio.png",
-    alt: "Pro-folio",
-    title: "Pro-folio",
-    description:
-      "Pro-folio: Personal project management application that will create a way for our team, as well as other developers, to market themselves through an app designed to showcase your projects all on one main page built using the MERN stack.",
-    github: "https://github.com/dev-rome/project3_frontend",
-    live: "https://pro-folio-njt.netlify.app/",
   },
 ];
