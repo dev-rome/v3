01dc6644193f164c2eecab868ceb407befab03a3
dev-rome <haynesjerome@gmail.com>
2023-09-13 16:20:45 -0400

    add animation for portfolio section

--- src/components/portfolio/Portfolio.tsx
+++ src/components/portfolio/Portfolio.tsx
@@ -1,22 +1,34 @@
 import { PortfolioData } from "./PortfolioData";
 import PortfolioItem from "./PortfolioItem";
 
+import { motion, AnimatePresence } from "framer-motion";
+import { useInView } from "react-intersection-observer";
+
 export default function Portfolio() {
+  const [ref, inView] = useInView({
+    triggerOnce: true,
+    threshold: 0.2,
+  });
 
   return (
-    <section id="portfolio" className="pb-32 lg:pb-64">
+    <section id="portfolio" className="pb-32 lg:pb-64" ref={ref}>
       <h2 className="text-[#0D4373] dark:text-[#7AB9F0] text-md mb-2">
         Portfolio
       </h2>
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3">
-        {PortfolioData.map((project) => {
-          return (
-            <PortfolioItem
+        <AnimatePresence>
+          {PortfolioData.map((project, index) => (
+            <motion.div
               key={project.id}
-              project={project}
-            />
-          );
-        })}
+              initial={{ opacity: 0, y: 100 }}
+              animate={inView ? { opacity: 1, y: 0 } : {}}
+              exit={{ opacity: 0, y: -100 }}
+              transition={{ duration: 0.5, delay: inView ? index * 0.4 : 0 }}
+            >
+              <PortfolioItem project={project} />
+            </motion.div>
+          ))}
+        </AnimatePresence>
       </div>
     </section>
   );
