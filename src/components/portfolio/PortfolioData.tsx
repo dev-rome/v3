ee658404b443f504730b3c3094c5228ed866fcd2
dev-rome <haynesjerome@gmail.com>
2023-10-02 18:02:58 -0400

    remove projects

--- src/components/portfolio/PortfolioData.tsx
+++ src/components/portfolio/PortfolioData.tsx
@@ -1,16 +1,6 @@
 export const PortfolioData = [
   {
     id: 1,
-    src: "/images/Thread-Clone.png",
-    alt: "Threads-Clone",
-    title: "Threads-Clone",
-    description:
-      "Threads-Clone is a dynamic messaging platform reminiscent of Threads. Built with Next.js, TypeScript, MongoDB, and Tailwind CSS ",
-    github: "https://github.com/dev-rome/Threads-Clone",
-    live: "https://threads-clone-red-pi.vercel.app/",
-  },
-  {
-    id: 2,
     src: "/images/little-lemon.png",
     alt: "Little Lemon",
     title: "Little Lemon",
@@ -20,27 +10,7 @@ export const PortfolioData = [
     live: "https://little-lemon-restaurant-eako.vercel.app/",
   },
   {
-    id: 3,
-    src: "/images/CarHub.png",
-    alt: "CarHub",
-    title: "CarHub",
-    description:
-      "CarHub is a Next.js web application built with Tailwind CSS and TypeScript. It utilizes the rapidapi cars by API-Ninjas to enable users to browse and search for popular cars in a simple and convenient manner.",
-    github: "https://github.com/dev-rome/Car-Showcase",
-    live: "https://car-showcase-2hjsy4mxb-dev-rome.vercel.app/",
-  },
-  {
-    id: 4,
-    src: "/images/prompts.png",
-    alt: "Prompts",
-    title: "Prompts",
-    description:
-      "Prompts is a web app developed with Next.js, Tailwind CSS, JavaScript, MongoDB, and NextAuth.js. It enables users to manage prompts (CRUD) and save favorites by creating accounts.",
-    github: "https://github.com/dev-rome/Prompts",
-    live: "https://prompts-dh3d5ui5f-dev-rome.vercel.app/",
-  },
-  {
-    id: 5,
+    id: 2,
     src: "/images/timber&hatchet.png",
     alt: "Timber and Hatchet",
     title: "Timber and Hatchet",
@@ -50,7 +20,7 @@ export const PortfolioData = [
     live: "https://astro-website-v1.netlify.app",
   },
   {
-    id: 6,
+    id: 3,
     src: "/images/hbo-stream.png",
     alt: "HBO Stream",
     title: "HBO Stream",
@@ -60,27 +30,7 @@ export const PortfolioData = [
     live: "https://aesthetic-alpaca-95ce00.netlify.app",
   },
   {
-    id: 7,
-    src: "/images/dice-game.png",
-    alt: "Dice Game",
-    title: "Dice Game",
-    description:
-      "The Dice Game is a simple web-based game built using HTML, CSS, and JavaScript with DOM manipulation. In this project, two players take turns rolling a dice, and the player with the highest score at the end of the game wins.",
-    github: "https://github.com/dev-rome/Dice_Game",
-    live: "https://glittery-souffle-ec064a.netlify.app/",
-  },
-  {
-    id: 8,
-    src: "/images/guess-my-number.png",
-    alt: "Guess My Number",
-    title: "Guess My Number",
-    description:
-      "The Number Guessing Game is a web-based game built using HTML, CSS, and JavaScript with DOM manipulation. The player is tasked with guessing a random number between 1 and 20. ",
-    github: "https://github.com/dev-rome/Guess-My-Number",
-    live: "https://marvelous-baklava-f9e655.netlify.app/",
-  },
-  {
-    id: 9,
+    id: 4,
     src: "/images/video-game-trivia.png",
     alt: "Video Game Trivia",
     title: "Video Game Trivia",
